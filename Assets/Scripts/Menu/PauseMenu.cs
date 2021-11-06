9499cebabbd9b3099592e5d554aae73529a038d4
Xwilarg <xwilarg@yahoo.fr>
2021-11-06 18:20:22 +0100

    Add player manager

--- Assets/Scripts/Menu/PauseMenu.cs
+++ Assets/Scripts/Menu/PauseMenu.cs
@@ -4,14 +4,7 @@ namespace Bug.Menu
 {
     public class PauseMenu : MonoBehaviour
     {
-        public static PauseMenu S;
         private GameObject _target;
-
-        private void Awake()
-        {
-            S = this;
-        }
-
         private void Start()
         {
             _target = transform.GetChild(0).gameObject;
