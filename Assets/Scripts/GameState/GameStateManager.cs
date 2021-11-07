0524626df88df75cf1c5259275c21fe810506a42
MadStark <antoine.fougea@gmail.com>
2021-11-07 12:01:10 +0100

    Fix paused state in Editor

--- Assets/Scripts/GameState/GameStateManager.cs
+++ Assets/Scripts/GameState/GameStateManager.cs
@@ -1,22 +1,39 @@
 ﻿using System;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif // UNITY_EDITOR
 
 namespace Bug
 {
 	public static class GameStateManager
 	{
 		private static bool _paused;
+		public static bool Paused { get => _paused; set => SetPaused(value); }
+		public static event Action<bool> OnPauseStateChanged;
 
-		public static bool Paused {
-			get => _paused;
-			set {
-				if (_paused != value)
-				{
-					_paused = value;
-					OnPauseStateChanged?.Invoke(_paused);
-				}
-			}
+
+#if UNITY_EDITOR
+		static GameStateManager()
+		{
+			EditorApplication.playModeStateChanged += HandlePlayModeStateChanged;
 		}
 
-		public static event Action<bool> OnPauseStateChanged;
+		private static void HandlePlayModeStateChanged(PlayModeStateChange stateChange)
+		{
+			if (stateChange is PlayModeStateChange.ExitingPlayMode or PlayModeStateChange.EnteredPlayMode)
+			{
+				_paused = false;
+			}
+		}
+#endif // UNITY_EDITOR
+
+		public static void SetPaused(bool paused)
+		{
+			if (_paused != paused)
+			{
+				_paused = paused;
+				OnPauseStateChanged?.Invoke(_paused);
+			}
+		}
 	}
 }
