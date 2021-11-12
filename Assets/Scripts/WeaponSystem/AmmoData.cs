a91391963245c37efc3db1fb3f72270431541c59
MadStark <antoine.fougea@gmail.com>
2021-11-12 01:33:49 +0100

    Add ammo to fire arms

---
+++ Assets/Scripts/WeaponSystem/AmmoData.cs
@@ -0,0 +1,16 @@
+﻿namespace Bug.WeaponSystem
+{
+	public class AmmoData
+	{
+		public int Capacity { get; set; }
+
+		public int Count { get; set; }
+
+
+		public AmmoData(int capacity, int count)
+		{
+			Capacity = capacity;
+			Count = count;
+		}
+	}
+}
---
+++ Assets/Scripts/WeaponSystem/FireArm.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Bug.WeaponSystem
+{
+	public abstract class FireArm : MonoBehaviour, IWeapon
+	{
+		[SerializeField] private int _ammoCapacity;
+		[SerializeField] private float _reloadDuration;
+		[SerializeField] private float _fireRate = 5f;
+		[SerializeField] private int _animationIndex;
+
+		public abstract bool CanHandlePrimaryAction { get; }
+
+		public abstract bool CanHandleSecondaryAction { get; }
+
+		public abstract bool CanReload { get; }
+
+		public AmmoData Ammo { get; private set; }
+
+
+		protected virtual void Awake()
+		{
+			Ammo = new AmmoData(_ammoCapacity, 0);
+		}
+
+		public abstract IEnumerator HandlePrimaryAction();
+
+		public abstract IEnumerator HandleSecondaryAction();
+
+		public abstract IEnumerator HandleReload();
+	}
+}
