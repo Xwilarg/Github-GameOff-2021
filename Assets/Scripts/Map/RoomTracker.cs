f3e578fd96a84069b0043d0c928be05642413db9
MadStark <antoine.fougea@gmail.com>
2021-11-30 09:59:05 +0100

    Add spawning system

---
+++ Assets/Scripts/Map/RoomTracker.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Bug.Map
+{
+	public class RoomTracker : MonoBehaviour
+	{
+		public Room CurrentRoom => _roomStack.LastOrDefault();
+
+		private List<Room> _roomStack = new List<Room>();
+
+
+		public void OnEnterRoom(Room room)
+		{
+			if (!_roomStack.Contains(room))
+				_roomStack.Add(room);
+		}
+
+		public void OnExitRoom(Room room)
+		{
+			if (_roomStack.Contains(room))
+				_roomStack.Remove(room);
+		}
+	}
+}
---
+++ Assets/Scripts/SpawnSystem/Spawner.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Bug.SpawnSystem
+{
+	public class Spawner : MonoBehaviour
+	{
+		[SerializeField] private GameObject[] _spawnPrefabs;
+		[SerializeField] private float _spawnCooldown = 60f;
+		[Range(0f, 1f)]
+		[SerializeField] private float _probability = 0.8f;
+
+		private GameObject _instance;
+		private float _lastSpawnTime;
+
+
+		private void Start()
+		{
+			_lastSpawnTime = -_spawnCooldown;
+		}
+
+		public void SpawnIfPossible()
+		{
+			if (_instance == null && Time.time > _lastSpawnTime + _spawnCooldown)
+			{
+				Spawn();
+			}
+		}
+
+		public void Spawn()
+		{
+			if (Random.value <= _probability)
+			{
+				GameObject prefab = GetRandomPrefab();
+				_instance = Instantiate(prefab, transform.position, Quaternion.identity);
+			}
+			_lastSpawnTime = Time.time;
+		}
+
+		private GameObject GetRandomPrefab()
+		{
+			return _spawnPrefabs[Random.Range(0, _spawnPrefabs.Length)];
+		}
+	}
+}
